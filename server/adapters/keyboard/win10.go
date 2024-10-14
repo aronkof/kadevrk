1ea313c562b070e0c445d54dbf2b0662578d4f22
Aron Koffler <aron.koffler@ifood.com.br>
2024-10-14 03:45:24 -0300

    finished implementing windows-10 translation

--- server/adapters/keyboard/win10.go
+++ server/adapters/keyboard/win10.go
@@ -1,23 +1,27 @@
 package keyboard
 
 import (
+	"fmt"
+
 	"github.com/aronkof/kadev-rk/core"
+	"github.com/aronkof/kadev-rk/foundation"
 	"github.com/bendahl/uinput"
 )
 
 func win10Translate(code int) (core.KeyStroke, error) {
 	switch code {
 	// ---- modifiers ---- //
+	case 91:
+		return core.KeyStroke{Code: uinput.KeyLeftmeta, IsModifier: false}, nil
 	case 160:
 		return core.KeyStroke{Code: uinput.KeyLeftshift, IsModifier: true}, nil
 	case 162:
 		return core.KeyStroke{Code: uinput.KeyLeftctrl, IsModifier: true}, nil
 	case 164:
 		return core.KeyStroke{Code: uinput.KeyLeftalt, IsModifier: true}, nil
-	case 91:
-		return core.KeyStroke{Code: uinput.KeyLeftmeta, IsModifier: false}, nil
 	// ---- modifiers ---- //
 
+	// ---- non alphanumeric ---- //
 	case 8:
 		return core.KeyStroke{Code: uinput.KeyBackspace, IsModifier: false}, nil
 	case 9:
@@ -38,6 +42,36 @@ func win10Translate(code int) (core.KeyStroke, error) {
 		return core.KeyStroke{Code: uinput.KeyEnd, IsModifier: false}, nil
 	case 36:
 		return core.KeyStroke{Code: uinput.KeyHome, IsModifier: false}, nil
+	case 45:
+		return core.KeyStroke{Code: uinput.KeyInsert, IsModifier: false}, nil
+	case 46:
+		return core.KeyStroke{Code: uinput.KeyDelete, IsModifier: false}, nil
+
+	case 186:
+		return core.KeyStroke{Code: uinput.KeySemicolon, IsModifier: false}, nil
+	case 187:
+		return core.KeyStroke{Code: uinput.KeyEqual, IsModifier: false}, nil
+	case 189:
+		return core.KeyStroke{Code: uinput.KeyMinus, IsModifier: false}, nil
+	case 220:
+		return core.KeyStroke{Code: uinput.KeyBackslash, IsModifier: false}, nil
+	case 191:
+		return core.KeyStroke{Code: uinput.KeySlash, IsModifier: false}, nil
+	case 188:
+		return core.KeyStroke{Code: uinput.KeyComma, IsModifier: false}, nil
+	case 190:
+		return core.KeyStroke{Code: uinput.KeyDot, IsModifier: false}, nil
+	case 219:
+		return core.KeyStroke{Code: uinput.KeyLeftbrace, IsModifier: false}, nil
+	case 221:
+		return core.KeyStroke{Code: uinput.KeyRightbrace, IsModifier: false}, nil
+	case 192:
+		return core.KeyStroke{Code: uinput.KeyGrave, IsModifier: false}, nil
+	case 222:
+		return core.KeyStroke{Code: uinput.KeyApostrophe, IsModifier: false}, nil
+	// ---- non alphanumeric ---- //
+
+	// ---- arrows ---- //
 	case 37:
 		return core.KeyStroke{Code: uinput.KeyLeft, IsModifier: false}, nil
 	case 38:
@@ -46,10 +80,9 @@ func win10Translate(code int) (core.KeyStroke, error) {
 		return core.KeyStroke{Code: uinput.KeyRight, IsModifier: false}, nil
 	case 40:
 		return core.KeyStroke{Code: uinput.KeyDown, IsModifier: false}, nil
-	case 45:
-		return core.KeyStroke{Code: uinput.KeyInsert, IsModifier: false}, nil
-	case 46:
-		return core.KeyStroke{Code: uinput.KeyDelete, IsModifier: false}, nil
+	// ---- arrows ---- //
+
+	// ---- alphanumeric ---- //
 	case 48:
 		return core.KeyStroke{Code: uinput.Key0, IsModifier: false}, nil
 	case 49:
@@ -70,7 +103,6 @@ func win10Translate(code int) (core.KeyStroke, error) {
 		return core.KeyStroke{Code: uinput.Key8, IsModifier: false}, nil
 	case 57:
 		return core.KeyStroke{Code: uinput.Key9, IsModifier: false}, nil
-
 	case 65:
 		return core.KeyStroke{Code: uinput.KeyA, IsModifier: false}, nil
 	case 66:
@@ -123,42 +155,35 @@ func win10Translate(code int) (core.KeyStroke, error) {
 		return core.KeyStroke{Code: uinput.KeyY, IsModifier: false}, nil
 	case 90:
 		return core.KeyStroke{Code: uinput.KeyZ, IsModifier: false}, nil
+	// ---- alphanumeric ---- //
+
+	// ---- FN keys ---- //
+	case 112:
+		return core.KeyStroke{Code: uinput.KeyF1, IsModifier: false}, nil
+	case 113:
+		return core.KeyStroke{Code: uinput.KeyF2, IsModifier: false}, nil
+	case 114:
+		return core.KeyStroke{Code: uinput.KeyF3, IsModifier: false}, nil
+	case 115:
+		return core.KeyStroke{Code: uinput.KeyF4, IsModifier: false}, nil
+	case 116:
+		return core.KeyStroke{Code: uinput.KeyF5, IsModifier: false}, nil
+	case 117:
+		return core.KeyStroke{Code: uinput.KeyF6, IsModifier: false}, nil
+	case 118:
+		return core.KeyStroke{Code: uinput.KeyF7, IsModifier: false}, nil
+	case 119:
+		return core.KeyStroke{Code: uinput.KeyF8, IsModifier: false}, nil
+	case 120:
+		return core.KeyStroke{Code: uinput.KeyF9, IsModifier: false}, nil
+	case 121:
+		return core.KeyStroke{Code: uinput.KeyF10, IsModifier: false}, nil
+	case 122:
+		return core.KeyStroke{Code: uinput.KeyF11, IsModifier: false}, nil
+	case 123:
+		return core.KeyStroke{Code: uinput.KeyF12, IsModifier: false}, nil
+	default:
+		return core.KeyStroke{}, fmt.Errorf("%d %w", code, foundation.KeyCodeNotFoundErr)
 	}
-
-	// VK_MULTIPLY	106	Multiply key
-	// VK_ADD	107	Add key
-	// VK_SEPARATOR	108	Separator key
-	// VK_SUBTRACT	109	Subtract key
-	// VK_DECIMAL	110	Decimal key
-	// VK_DIVIDE	111	Divide key
-
-	// VK_F1	112	F1 key
-	// VK_F2	113	F2 key
-	// VK_F3	114	F3 key
-	// VK_F4	115	F4 key
-	// VK_F5	116	F5 key
-	// VK_F6	117	F6 key
-	// VK_F7	118	F7 key
-	// VK_F8	119	F8 key
-	// VK_F9	120	F9 key
-	// VK_F10	121	F10 key
-	// VK_F11	122	F11 key
-	// VK_F12	123	F12 key
-	// VK_F13	124	F13 key
-	// VK_F24	135	F24 key
-
-	// VK_OEM_1	186	Used for miscellaneous characters; varies by keyboard. For the US standard keyboard, the ;: key
-	// VK_OEM_PLUS	187	For any country/region, the + key
-	// VK_OEM_COMMA	188	For any country/region, the , key
-	// VK_OEM_MINUS	189	For any country/region, the - key
-	// VK_OEM_PERIOD	190	For any country/region, the . key
-	// VK_OEM_2	191	Used for miscellaneous characters; varies by keyboard. For the US standard keyboard, the /? key
-	// VK_OEM_3	192	Used for miscellaneous characters; varies by keyboard. For the US standard keyboard, the `~ key
-	// VK_OEM_4	219	Used for miscellaneous characters; varies by keyboard. For the US standard keyboard, the [ { key
-	// VK_OEM_5	220	Used for miscellaneous characters; varies by keyboard. For the US standard keyboard, the | key
-	// VK_OEM_6	221	Used for miscellaneous characters; varies by keyboard. For the US standard keyboard, the ] } key
-	// VK_OEM_7	222	Used for miscellaneous characters; varies by keyboard. For the US standard keyboard, the ' " key
-	// VK_OEM_8	223	Used for miscellaneous characters; varies by keyboard.
-
-	return core.KeyStroke{}, nil
+	// ---- FN keys ---- //
 }
